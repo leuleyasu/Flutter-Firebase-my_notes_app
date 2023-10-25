c7db9af27a0db7199792f11a94a2228f1ac36afa
leuleyasu <leuleyasu7@gmail.com>
2023-10-25 16:15:58 +0300

    feat: making simple test in authprovider

--- lib/view/Home.dart
+++ lib/view/Home.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/services/auth/auth_service.dart';
 import 'package:flutter_application_1/view/Notes.dart';
-import 'package:flutter_application_1/view/login.dart';
 import 'package:flutter_application_1/view/verifyemail.dart';
 import 'dart:developer' as developertool show log;
-import '../firebase_options.dart';
-
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -18,33 +14,30 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return  FutureBuilder(
-       future: AuthService.firbase().initializeApp(),
-        builder: (context, snapshot) {
-switch(snapshot.connectionState){
-  case ConnectionState.done:
- final user= AuthService.firbase().currentUser;
- if(user!=null){
- if(user.isEmailVerfied){
-  //  ScaffoldMessenger.of(context).
-  // showSnackBar(const
-  // SnackBar(content: Text("Welcome")));
-  return const NotesPage();
+    return FutureBuilder(
+      future: AuthService.firbase().initializeApp(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firbase().currentUser;
+            if (user != null) {
+              if (user.isEmailVerfied) {
+                //  ScaffoldMessenger.of(context).
+                // showSnackBar(const
+                // SnackBar(content: Text("Welcome")));
+                return const NotesPage();
+              } else {
+                return const VerifyEmail();
+              }
+            }
 
- }else {
-return const VerifyEmail();
+            developertool.log(user.toString());
 
- }
- }
-
- developertool.log(user.toString());
-
-  default:return const Text("waiting for connection");
-
-}return const Text("something went wrong");
-        },
-
-      );
-
+          default:
+            return const Text("waiting for connection");
+        }
+        return const Text("something went wrong");
+      },
+    );
   }
 }
